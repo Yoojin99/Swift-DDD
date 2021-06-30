f586ae708d20931f7b7fec112223bdcb1569ab7f
Yoojin <y00jin@nhnpayco.com>
2021-06-30 17:42:12 +0900

    Data local에 저장하기 구현(Repository 생성 x)

--- DDD/Domain/Service/UserService.swift
+++ DDD/Domain/Service/UserService.swift
@@ -6,10 +6,30 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
 
 class UserService {
-    func isNameDuplicate(userModel: UserModel) -> Bool {
+    static func isNameDuplicate(userName: String) -> Bool {
         // check whether name is duplicate
+        
+        let viewController = UIApplication.shared.windows.first!.rootViewController as! ViewController
+        let context = viewController.persistentContainer.viewContext
+        let query: NSFetchRequest<User> = User.fetchRequest()
+        
+        let predicate = NSPredicate(format: "name contains[c] %@", userName)
+        query.predicate = predicate
+
+        do{
+            let users = try context.fetch(query)
+            
+            if users.count > 0 {
+                return true
+            }
+        }catch{
+            print("error")
+        }
+        
         return false
     }
 }
