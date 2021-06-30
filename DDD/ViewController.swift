13f17045f7e028d3120e086ad2ea83476bfc32ac
Yoojin <y00jin@nhnpayco.com>
2021-06-30 16:45:53 +0900

    ViewController에 data 저장하는 메서드 추가

--- DDD/ViewController.swift
+++ DDD/ViewController.swift
@@ -6,34 +6,68 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    // MARK: coreData
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         setUpView()
-        
-        var user1: UserModel
-        var user2: UserModel
-        
-        do {
-            user1 = try UserModel(id: "id1", name: "hii")
-            user2 = try UserModel(id: "id1", name: "Hii")
-            
-            print(user1.equals(user: user2))
-            print(user1 == user2)
-
-        } catch {
-            print(error)
-        }
-        
     }
-
 
     private func setUpView() {
         view.backgroundColor = .white
+    }
+    
+    // MARK: coreData saving support
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    // MARK: data handling
+    private func createUser(id: String, name: String) throws {
+        let userModel: UserModel = try UserModel(id: id, name: name)
+        let userService: UserService = UserService()
+        
+        if userService.isNameDuplicate(userModel: userModel) {
+            throw ExceptionError.ExceptionError("중복되는 이름")
+        }
+        
+        let context = persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: context)
+        
+        if let entity = entity {
+            let user = NSManagedObject(entity: entity, insertInto: context)
+            user.setValue(userModel.userId.value, forKey: "id")
+            user.setValue(userModel.name, forKey: "name")
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 //
