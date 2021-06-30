61c8e773089016f0f91fe8a31733b1ca1f850ae4
Yoojin <y00jin@nhnpayco.com>
2021-06-30 15:16:05 +0900

    UserService 생성

---
+++ DDD/Domain/Service/UserService.swift
@@ -0,0 +1,15 @@
+//
+//  UserService.swift
+//  DDD
+//
+//  Created by NHN on 2021/06/30.
+//
+
+import Foundation
+
+class UserService {
+    func isNameDuplicate() -> Bool {
+        // check whether name is duplicate
+        return false
+    }
+}
